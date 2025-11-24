ab20e2cdf7594bd131f95d81863493e04fc2cd38
miguel-jaeger <miguel.jaeger@gmail.com>
2025-11-24 16:13:10 -0500

    Creado controlador

--- app/src/main/java/com/log/app/AppApplication.java
+++ app/src/main/java/com/log/app/AppApplication.java
@@ -1,13 +1,38 @@
 package com.log.app;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(AppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		logger.info("La aplicación ha iniciado exitosamente.");
 	}
 
+	/**
+	 ** Endpoint que demuestra el uso de diferentes niveles de registro.
+	 * 
+	 */
+
+	@GetMapping("/api/demo-logs")
+	public String demoLogs() {
+		logger.trace("Este es un mensaje de TRACE.");
+		logger.debug("Este es un mensaje de DEBUG.");
+
+		logger.info("Se ha recibido una petición al endpoint /api/demo-logs.");
+		logger.warn("Advertencia: Posible cuello de botella en la operación.");
+		try {
+			int resultado = 10 / 0;
+		} catch (Exception e) {
+			logger.error("Error: Se ha intentado una división por cero.", e);
+		}
+		return "Revisa la consola para ver los logs.";
+	}
 }
